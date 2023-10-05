83fe0805afe60840facb6d81e15f403e2bb80365
skeesh24 <d.pacooloff@gmail.com>
2023-10-05 17:38:55 +0500

    add the entrypoint syntax

--- main.py
+++ main.py
@@ -22,10 +22,6 @@ def send_email(to: str, subject: str, msg: str) -> None:
         server.quit()
 
 
-prepare: dict = lambda msg: literal_eval(msg.decode("utf-8"))
-initialize = lambda host: RedisConsumer(host=host)
-
-
 def rabbitmq_queue_callback(ch, method, properties, body):
     send_email(**prepare(body))
 
@@ -34,12 +30,18 @@ def redis_queue_callback(body):
     send_email(**prepare(body))
 
 
-consumer = initialize(sett.BROKER_HOST)
+if __name__ == "__main__":
+    print("[*] Starting service")
+    prepare: dict = lambda msg: literal_eval(msg.decode("utf-8"))
+    initialize = lambda host: RedisConsumer(host=host)
+    print("[*] Initialization started")
+    consumer = initialize(sett.BROKER_HOST)
+    print("[*] Initialization ended")
 
-try:
-    print("[*] consuming started")
-    consumer.start_consuming(redis_queue_callback)
-except KeyboardInterrupt:
-    print("Interrupted")
-    consumer.stop_consuming()
-    consumer.close()
+    try:
+        print("[*] consuming started")
+        consumer.start_consuming(redis_queue_callback)
+    except KeyboardInterrupt:
+        print("Interrupted")
+        consumer.stop_consuming()
+        consumer.close()
