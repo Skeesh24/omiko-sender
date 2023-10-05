8e919aa420b320fa17369453b0e986c540743cd5
skeesh24 <d.pacooloff@gmail.com>
2023-10-05 14:22:47 +0500

    add some refactoring

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import smtplib as sender
-from os import environ
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 from ast import literal_eval
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 import pika
-from os import environ
 
 from settings import sett
 
@@ -25,13 +24,24 @@ def send_email(to: str, subject: str, msg: str) -> None:
 
 
 def queue_callback(ch, method, properties, body):
-    send_email(**literal_eval(body.decode("utf-8").replace("`", '"')))
+    prepare: dict = lambda msg: literal_eval(msg.decode("utf-8"))
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=sett.BROKER_HOST))
-channel = connection.channel()
-q_name = sett.RECOVERY_QUEUE
-channel.queue_declare(q_name)
-channel.basic_consume(q_name, on_message_callback=queue_callback, auto_ack=True)
+    send_email(**prepare(body))
+
+
+def initialize():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=sett.BROKER_HOST)
+    )
+    channel = connection.channel()
+    channel.queue_declare(sett.RECOVERY_QUEUE)
+    channel.basic_consume(
+        sett.RECOVERY_QUEUE, on_message_callback=queue_callback, auto_ack=True
+    )
+    return connection, channel
+
+
+connection, channel = initialize()
 
 try:
     print("[*] consuming started")
