79eb6fa71e519ac594d1e927d0513a7d6fc9cac6
skeesh24 <d.pacooloff@gmail.com>
2023-10-07 18:50:44 +0500

    refactoring main

--- main.py
+++ main.py
@@ -3,49 +3,53 @@ from ast import literal_eval
 from asyncio import run
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from os import environ
 from threading import Thread
 
-from redis import Redis
+from interface import IBroker
+from services import RedisBroker
+from settings import sett
 
 
 def send_email(to: str, subject: str, msg: str) -> None:
-    with sender.SMTP(environ.get("SENDER_HOST"), environ.get("SENDER_PORT")) as server:
+    with sender.SMTP(sett.SENDER_HOST, sett.SENDER_PORT) as server:
         server.starttls()
-        server.login(environ.get("SENDER_USERNAME"), environ.get("SENDER_ACC"))
+        server.login(sett.SENDER_USERNAME, sett.SENDER_ACC)
 
         message = MIMEMultipart()
-        message["From"] = environ.get("SENDER_FROM")
+        message["From"] = sett.SENDER_FROM
         message["To"] = to
         message["Subject"] = subject
         message.attach(MIMEText(msg, "html"))
 
-        server.sendmail(environ.get("SENDER_FROM"), to, message.as_string())
+        server.sendmail(sett.SENDER_FROM, to, message.as_string())
         server.quit()
 
 
-def queue_callback(body):
+def queue_callback(body: bytes) -> None:
     send_email(**literal_eval(body.decode("utf-8")))
     print("email sent")
 
 
-async def consuming(connection):
+async def consuming(connection: IBroker):
     while True:
-        message = connection.lpop(environ.get("RECOVERY_QUEUE"))
-        if message: queue_callback(message)
+        message = connection.get_message()
+        if message:
+            queue_callback(message)
 
 
-def unblocking_run(connection):
+def unblocking_run(connection: IBroker):
     run(consuming(connection))
 
 
 if __name__ == "__main__":
     print("connect to the broker...")
-    connection: Redis = Redis.from_url(url=environ.get("BROKER_HOST"))
+    consumer = RedisBroker()
+    consumer.connect(sett.BROKER_HOST)
     print("connection passed")
-    q_name = environ.get("RECOVERY_QUEUE")
+
     print("queue declaration")
-    connection.pubsub().subscribe(**{q_name: queue_callback})
+    consumer.subscribe(sett.RECOVERY_QUEUE, queue_callback)
     print("subscribed")
+
     print("consume initialized")
-    Thread(target=unblocking_run, args=(connection,)).start()
+    Thread(target=unblocking_run, args=(consumer,)).start()
