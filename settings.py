62a61f1b85e6bb778b74101aa4a504dacca83e57
skeesh24 <d.pacooloff@gmail.com>
2023-10-16 18:30:13 +0500

    add nissing env

--- settings.py
+++ settings.py
@@ -12,3 +12,4 @@ class sett:
     SENDER_ACC: str = env("SENDER_ACC")
     SENDER_FROM: str = env("SENDER_FROM")
     RECOVERY_QUEUE: str = env("RECOVERY_QUEUE")
+    BROKER_HOST: str = env("BROKER_HOST")
